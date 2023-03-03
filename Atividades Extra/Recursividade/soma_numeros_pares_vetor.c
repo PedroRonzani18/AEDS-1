a58070332bb935c205e90a00df3ff442188c519a
Pedro Augusto de Portilho Ronzani <pedroaugustogabironzani@hotmail.com>
2023-03-03 08:52:16 -0300

    Initial commit

---
+++ Atividades Extra/Recursividade/soma_numeros_pares_vetor.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int positivos(int v[], int n, int* soma)
+{
+    if(n == 1)
+    {
+        if(n % 2 == 0) 
+        {
+            soma += v[n-1];
+            return v[n-1];
+        }
+        return 0;
+    }
+
+    positivos(v, n-1, soma);
+
+    if(v[n-1] % 2 == 0) {*soma += v[n-1];}
+
+    return *soma;
+}
+
+int soma_positivos(int v[], int n)
+{
+    int soma = 0;
+
+    return positivos(v, n, &soma);
+}
+
+
+int main()
+{
+    int v[] = {1,2,3,4,5,6};
+    int n = sizeof(v) / sizeof(int);
+    printf("%d\n",soma_positivos(v,n));
+}
